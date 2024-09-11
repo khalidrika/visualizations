695f7921911ca6a8cdad159c6ae06833113a9515
khalidrika <rikarikamika2002@gmail.com>
2024-09-11 18:15:56 +0100

    style

--- Handlers/StyleHandler.go
+++ Handlers/StyleHandler.go
@@ -21,7 +21,7 @@ import (
 // Handle serving css content, while blocking access to paths "/css/..."
 func StyleHandler(w http.ResponseWriter, r *http.Request) {
 	// Strip the "/css/" prefix from the URL path to get the relative file path
-	a := "style/styles/" + strings.TrimPrefix(r.URL.Path, "/style/")
+	a := "style/" + strings.TrimPrefix(r.URL.Path, "/style/")
 	// Read the file from the embedded filesystem
 
 	b, err := os.ReadFile(a)
